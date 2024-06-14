909fa8ea418666d3fb3add757784ba0b0c84a13a
BlarkLee <blarklee666@gmail.com>
2024-06-14 14:47:43 -0700

    delete

--- models/NeRF/monodepth2/depth_decoder_light.py
+++ models/NeRF/monodepth2/depth_decoder_light.py
@@ -82,7 +82,6 @@ class DepthDecoder(nn.Module):
             num_ch_in = self.num_ch_enc[-1] if i == 2 else self.num_ch_dec[i + 1]
             num_ch_out = self.num_ch_dec[i]
             self.convs[self.tuple_to_str(("upconv", i, 0))] = ConvBlock(num_ch_in, num_ch_out)
-            #print("upconv_{}_{}".format(i, 0), num_ch_in, num_ch_out)
 
             # upconv_1
             num_ch_in = self.num_ch_dec[i]
@@ -90,7 +89,6 @@ class DepthDecoder(nn.Module):
                 num_ch_in += self.num_ch_enc[i - 1]
             num_ch_out = self.num_ch_dec[i]
             self.convs[self.tuple_to_str(("upconv", i, 1))] = ConvBlock(num_ch_in, num_ch_out)
-            #print("upconv_{}_{}".format(i, 1), num_ch_in, num_ch_out)
 
         for s in self.scales:
             self.convs[self.tuple_to_str(("dispconv", s))] = Conv3x3(self.num_ch_dec[s], self.num_output_channels)
@@ -101,37 +99,23 @@ class DepthDecoder(nn.Module):
     def forward(self, input_features, disparity):
         input_features = list(input_features)
         B, S = disparity.size()
-        #print("disparity", disparity.shape)
         disparity = self.embedder(disparity.reshape(B * S, 1)).unsqueeze(2).unsqueeze(3)
-        #print("disparity", disparity.shape)
 
         # extension of encoder to increase receptive field
         input_features = input_features[:3]
-        '''
-        for i in input_features:
-            print("input_features", i.shape)'''
-
         encoder_out = input_features[-1]
-        #print("encoder_out", encoder_out.shape)
         conv_down1 = self.conv_down1(self.downsample(encoder_out))
-        #print("conv_down1", conv_down1.shape)
         conv_down2 = self.conv_down2(self.downsample(conv_down1))
-        #print("conv_down2", conv_down2.shape)
         conv_up1 = self.conv_up1(self.upsample(conv_down2))
-        #print("conv_up1", conv_up1.shape)
         conv_up2 = self.conv_up2(self.upsample(conv_up1))
-        #print("conv_up2", conv_up2.shape)
         conv_up2 = F.interpolate(conv_up2, size=(encoder_out.shape[-2], encoder_out.shape[-1]), mode="nearest")
-        #print("conv_up2", conv_up2.shape)
 
         # repeat / reshape features
         _, C_feat, H_feat, W_feat = conv_up2.size()
         feat_tmp = conv_up2.unsqueeze(1).expand(B, S, C_feat, H_feat, W_feat) \
             .contiguous().view(B * S, C_feat, H_feat, W_feat)
         disparity_BsCHW = disparity.repeat(1, 1, H_feat, W_feat)
-        #print("disparity_BsCHW", disparity_BsCHW.shape)
         conv_up2 = torch.cat((feat_tmp, disparity_BsCHW), dim=1)
-        #print("conv_up2", conv_up2.shape)
         
         # repeat / reshape features
         for i, feat in enumerate(input_features):
@@ -139,10 +123,7 @@ class DepthDecoder(nn.Module):
             feat_tmp = feat.unsqueeze(1).expand(B, S, C_feat, H_feat, W_feat) \
                 .contiguous().view(B * S, C_feat, H_feat, W_feat)
             disparity_BsCHW = disparity.repeat(1, 1, H_feat, W_feat)
-            #print("feat_tmp", feat_tmp.shape)
-            #print("disparity_BsCHW", disparity_BsCHW.shape)
             input_features[i] = torch.cat((feat_tmp, disparity_BsCHW), dim=1)
-            #print("input_features[i]", input_features[i].shape)
         
         # for i, feat in enumerate(input_features):
         #     _, C_feat, H_feat, W_feat = feat.size()
@@ -155,34 +136,24 @@ class DepthDecoder(nn.Module):
         x = conv_up2
         for i in range(2, -1, -1):
             x = self.convs[self.tuple_to_str(("upconv", i, 0))](x)
-            #print("x", x.shape)
             x = [upsample(x)]
-            #print("x[0]", x[0].shape)
             if self.use_skips and i > 0:
                 x += [input_features[i - 1]]
             x = torch.cat(x, 1)
-            #print("x", x.shape)
             x = self.convs[self.tuple_to_str(("upconv", i, 1))](x)
-            #print("x", x.shape)
             if i in self.scales:
                 output = self.convs[self.tuple_to_str(("dispconv", i))](x)
-                #print("output", output.shape)
                 H_mpi, W_mpi = output.size(2), output.size(3)
                 mpi = output.view(B, S, 4, H_mpi, W_mpi)
-                #print("mpi", mpi.shape)
                 mpi_rgb = self.sigmoid(mpi[:, :, 0:3, :, :])
-                #print("mpi_rgb", mpi_rgb.shape)
                 mpi_sigma = torch.abs(mpi[:, :, 3:, :, :]) + 1e-4 \
                         if not self.use_alpha \
                         else self.sigmoid(mpi[:, :, 3:, :, :])
-                #print("mpi_sigma", mpi_sigma.shape)
 
                 if self.sigma_dropout_rate > 0.0 and self.training:
                     mpi_sigma = F.dropout2d(mpi_sigma, p=self.sigma_dropout_rate)
                     
                 outputs[("disp", i)] = torch.cat((mpi_rgb, mpi_sigma), dim=2)
-                #print("outputs[('disp', i)]", i, outputs[("disp", i)].shape)
-                #print("outputs[(disp, i)]", outputs[("disp", i)].shape)
         #stop
 
         return outputs
